05d3c6592abf18bd93be41bd3ec20e84b1cd0118
Yani <jani_boy@abv.bg>
2017-06-29 22:29:42 +0300

    Labyrinth Escape AI
    C# Labyrinth Escape with AI

---
+++ LabyrinthEscapeAI/LabyrinthEscapeAI/LabyrinthForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace LabyrinthEscapeAI
+{
+    partial class LabyrinthForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labyrinthPanel = new System.Windows.Forms.Panel();
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.episodesLabel = new System.Windows.Forms.Label();
+            this.episodesCountLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labyrinthPanel
+            // 
+            this.labyrinthPanel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labyrinthPanel.Location = new System.Drawing.Point(12, 12);
+            this.labyrinthPanel.Name = "labyrinthPanel";
+            this.labyrinthPanel.Size = new System.Drawing.Size(501, 501);
+            this.labyrinthPanel.TabIndex = 0;
+            this.labyrinthPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.labyrinthPanel_Paint);
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.AutoSize = true;
+            this.messageLabel.Location = new System.Drawing.Point(519, 12);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(0, 13);
+            this.messageLabel.TabIndex = 2;
+            // 
+            // episodesLabel
+            // 
+            this.episodesLabel.AutoSize = true;
+            this.episodesLabel.Location = new System.Drawing.Point(518, 13);
+            this.episodesLabel.Name = "episodesLabel";
+            this.episodesLabel.Size = new System.Drawing.Size(53, 13);
+            this.episodesLabel.TabIndex = 3;
+            this.episodesLabel.Text = "Episodes:";
+            // 
+            // episodesCountLabel
+            // 
+            this.episodesCountLabel.AutoSize = true;
+            this.episodesCountLabel.Location = new System.Drawing.Point(577, 13);
+            this.episodesCountLabel.Name = "episodesCountLabel";
+            this.episodesCountLabel.Size = new System.Drawing.Size(0, 13);
+            this.episodesCountLabel.TabIndex = 4;
+            // 
+            // LabyrinthForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 540);
+            this.Controls.Add(this.episodesCountLabel);
+            this.Controls.Add(this.episodesLabel);
+            this.Controls.Add(this.messageLabel);
+            this.Controls.Add(this.labyrinthPanel);
+            this.Name = "LabyrinthForm";
+            this.Text = "Labyrinth";
+            this.Load += new System.EventHandler(this.LabyrinthForm_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.LabyrinthForm_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel labyrinthPanel;
+        private System.Windows.Forms.Label messageLabel;
+        private System.Windows.Forms.Label episodesLabel;
+        private System.Windows.Forms.Label episodesCountLabel;
+    }
+}
+
---
+++ LabyrinthEscapeAI/LabyrinthEscapeAI/LabyrinthForm.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabyrinthEscapeAI
+{
+    public partial class LabyrinthForm : Form
+    {
+        public LabyrinthForm()
+        {
+            InitializeComponent();
+            graphics = labyrinthPanel.CreateGraphics();
+            graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            jamesBond = new Agent(startCoordinates[0], startCoordinates[1]);
+            gridXWidth = labyrinthPattern[0].Length;
+            finalState = GetState(finishCoordinates[0], finishCoordinates[1]);
+            InitializeQTable();
+            episodesCountLabel.Text = episodes.ToString();
+        }
+
+        private int[][] labyrinthPattern = new int[][] { new int [] {0, 1, 1, 1, 1, 0, 1, 0, 0, 1},
+                                                         new int [] {0, 0, 1, 0, 0, 0, 0, 0, 0, 1},
+                                                         new int [] {0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+                                                         new int [] {1, 1, 0, 0, 0, 1, 0, 1, 1, 0},
+                                                         new int [] {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+                                                         new int [] {1, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+                                                         new int [] {1, 1, 0, 1, 0, 1, 0, 0, 0, 0},
+                                                         new int [] {0, 0, 0, 1, 0, 1, 0, 1, 0, 0},
+                                                         new int [] {1, 0, 1, 1, 0, 1, 0, 0, 0, 1},
+                                                         new int [] {0, 0, 1, 1, 1, 1, 0, 0, 1, 1}};
+
+        private decimal[,] Qtable = new decimal[100, 4];
+
+        private int[] startCoordinates = new int[] { 0, 9 };
+        private int[] finishCoordinates = new int[] { 5, 0 };
+        private int gridXWidth;
+
+        Graphics graphics;
+        Brush redBrush = new SolidBrush(Color.Red);
+        Brush greenBrush = new SolidBrush(Color.Green);
+        Brush pinkBrush = new SolidBrush(Color.Pink);
+        Brush grayBrush = new SolidBrush(Color.DarkGray);
+        Pen pen = new Pen(Color.DarkGray);
+        Agent jamesBond;
+        decimal discountFactor = 0.99M;
+        decimal alpha = 0.1M;
+        decimal epsilon = 0.1M;
+        int episodes = 0;
+        private int finalState;
+
+        public void DrawGrid()
+        {
+            int labyrinthWidth = labyrinthPanel.Width;
+            int labyrinthHeight = labyrinthPanel.Height;
+
+            int horizontalGridLinesCount = labyrinthPattern.Length;
+            int verticalGridLinesCount = labyrinthPattern[0].Length;
+            int marginBetweenHorizontalLines = (labyrinthHeight - horizontalGridLinesCount) / horizontalGridLinesCount;
+            int marginBetweenVerticalLines = (labyrinthWidth - verticalGridLinesCount) / verticalGridLinesCount;
+
+            horizontalGridLinesCount += 1;
+            verticalGridLinesCount += 1;
+
+            int currentX = 0;
+            int currentY = 0;
+            int lastX = labyrinthWidth - 1;
+            int lastY = labyrinthHeight - 1;
+
+            Graphics graphics = labyrinthPanel.CreateGraphics();
+            Brush redBrush = new SolidBrush(Color.Red);
+            Brush greenBrush = new SolidBrush(Color.Green);
+            Brush pinkBrush = new SolidBrush(Color.Pink);
+            Brush grayBrush = new SolidBrush(Color.DarkGray);
+            Pen pen = new Pen(Color.DarkGray);
+
+            for (int i = 0; i < horizontalGridLinesCount; i++)
+            {
+                graphics.DrawLine(pen, 0, currentY, lastX, currentY);
+                currentX = 0;
+
+                for (int j = 0; j < verticalGridLinesCount; j++)
+                {
+                    graphics.DrawLine(pen, currentX, 0, currentX, lastY);
+
+                    if (i < labyrinthPattern.Length && j < labyrinthPattern[i].Length && labyrinthPattern[i][j] == 1)
+                    {
+                        graphics.FillRectangle(grayBrush, currentX + 1, currentY + 1, marginBetweenVerticalLines, marginBetweenHorizontalLines);
+                    }
+
+                    if (j == startCoordinates[0] && i == startCoordinates[1])
+                    {
+                        graphics.FillRectangle(greenBrush, currentX + 1, currentY + 1, marginBetweenVerticalLines, marginBetweenHorizontalLines);
+                    }
+
+                    if (j == finishCoordinates[0] && i == finishCoordinates[1])
+                    {
+                        graphics.FillRectangle(pinkBrush, currentX + 1, currentY + 1, marginBetweenVerticalLines, marginBetweenHorizontalLines);
+                    }
+
+                    currentX += marginBetweenVerticalLines + 1;
+                }
+
+                currentY += marginBetweenHorizontalLines + 1;
+            }
+        }
+
+        public void MoveAgent(int direction)
+        {
+            //0 - up, 1 - down, 2 - left, 3 - right
+
+            switch (direction)
+            {
+                case 0:
+                    {
+                        if (jamesBond.AgentPosition.Y - 1 < 0 || labyrinthPattern[jamesBond.AgentPosition.Y - 1][jamesBond.AgentPosition.X] == 1)
+                        {
+                            break;
+                        }
+
+                        jamesBond.AgentPosition.Y -= 1;
+                        break;
+                    }
+                case 1:
+                    {
+                        if (jamesBond.AgentPosition.Y + 1 > 9 || labyrinthPattern[jamesBond.AgentPosition.Y + 1][jamesBond.AgentPosition.X] == 1)
+                        {
+                            break;
+                        }
+
+                        jamesBond.AgentPosition.Y += 1;
+                        break;
+                    }
+                case 2:
+                    {
+                        if (jamesBond.AgentPosition.X - 1 < 0 || labyrinthPattern[jamesBond.AgentPosition.Y][jamesBond.AgentPosition.X - 1] == 1)
+                        {
+                            break;
+                        }
+
+                        jamesBond.AgentPosition.X -= 1;
+                        break;
+                    }
+                case 3:
+                    {
+                        if (jamesBond.AgentPosition.X + 1 > 9 || labyrinthPattern[jamesBond.AgentPosition.Y][jamesBond.AgentPosition.X + 1] == 1)
+                        {
+                            break;
+                        }
+
+                        jamesBond.AgentPosition.X += 1;
+                        break;
+                    }
+            }
+        }
+
+        class Agent
+        {
+            public Agent(int x, int y)
+            {
+                AgentPosition = new Position(x, y);
+            }
+
+            public Position AgentPosition { get; set; }
+        }
+
+        class Position
+        {
+            public Position(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+
+        private void labyrinthPanel_Paint(object sender, PaintEventArgs e)
+        {
+            DrawGrid();
+            graphics.FillEllipse(redBrush, jamesBond.AgentPosition.X * 50 + 1, jamesBond.AgentPosition.Y * 50 + 1, 49, 49);
+        }
+
+        private void LabyrinthForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyValue)
+            {
+                case 38: MoveAgent(0); break;
+                case 40: MoveAgent(1); break;
+                case 37: MoveAgent(2); break;
+                case 39: MoveAgent(3); break;
+            }
+
+            labyrinthPanel.Refresh();
+        }
+
+        private int GetState(int x, int y)
+        {
+            return y * gridXWidth + x;
+        }
+
+        private Position GetPosition(int state)
+        {
+            return new Position(state % gridXWidth, state / gridXWidth);
+        }
+
+        private void InitializeQTable()
+        {
+            int finalState = finishCoordinates[1] * gridXWidth + finishCoordinates[0];
+
+            for (int s = 0; s < 100; s++)
+            {
+                for (int a = 0; a < 4; a++)
+                {
+                    Qtable[s, a] = 1.1M;
+                }
+            }
+
+            for (int a = 0; a < 4; a++)
+            {
+                Qtable[finalState, a] = 0;
+            }
+        }
+
+        private int SelectAction(int currentState)
+        {
+            Random random = new Random();
+            int rand = random.Next(0, 101);
+
+            if (rand <= 10)
+            {
+                return random.Next(0, 4);
+            }
+            else
+            {
+                decimal maxQ = Qtable[currentState, 0];
+                int maxA = 0;
+
+                for (int a = 1; a < 4; a++)
+                {
+                    if (Qtable[currentState, a] > maxQ)
+                    {
+                        maxQ = Qtable[currentState, a];
+                        maxA = a;
+                    }
+                }
+
+                return maxA;
+            }
+        }
+
+        private void TakeAction(int initialState, int action)
+        {
+            MoveAgent(action);
+            int nextState = GetState(jamesBond.AgentPosition.X, jamesBond.AgentPosition.Y);
+            int reward = 0;
+
+            if (nextState == finalState)
+            {
+                reward = 1;
+            }
+
+            decimal v = Qtable[nextState, 0];
+
+            for (int a = 1; a < 4; a++)
+            {
+                if (Qtable[nextState, a] > v)
+                {
+                    v = Qtable[nextState, a];
+                }
+            }
+
+            // Update QTable
+
+            Qtable[initialState, action] = (1 - alpha) * Qtable[initialState, action] + alpha * (reward + discountFactor * v);
+        }
+
+        private void ResetBoard()
+        {
+            jamesBond.AgentPosition.X = startCoordinates[0];
+            jamesBond.AgentPosition.Y = startCoordinates[1];
+
+            labyrinthPanel.Refresh();
+        }
+
+        private void LabyrinthForm_Load(object sender, EventArgs e)
+        {
+            Thread t = new Thread(Learn);
+            t.Start();
+        }
+
+        private void Learn()
+        {
+            while (episodes < 20000)
+            {
+                Application.DoEvents();
+                Thread.Sleep(100);
+
+                int initialState = GetState(jamesBond.AgentPosition.X, jamesBond.AgentPosition.Y);
+                int action = SelectAction(initialState);
+                TakeAction(initialState, action);
+                int nextState = GetState(jamesBond.AgentPosition.X, jamesBond.AgentPosition.Y);
+
+                if (nextState == finalState)
+                {
+                    episodes++;
+                    episodesCountLabel.Text = episodes.ToString();
+                    ResetBoard();
+                }
+            }
+        }
+    }
+}

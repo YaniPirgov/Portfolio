53225ca860e6b63a4cda7302920f728408543741
Yani <jani_boy@abv.bg>
2017-06-30 18:05:55 +0300

    Calculating Figures
    Calculating Figures with C#

---
+++ C# Projects/Calculating Figures/radioUpr/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace radioUpr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.txtBox3 = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lice = new System.Windows.Forms.Label();
+            this.perimetar = new System.Windows.Forms.Label();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.lblC = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(38, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(67, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Квадрат";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(329, 12);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(87, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Триъгълник";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(179, 12);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(100, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Правоъгълник";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Location = new System.Drawing.Point(68, 147);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(100, 20);
+            this.txtBox1.TabIndex = 3;
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.Location = new System.Drawing.Point(68, 185);
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.Size = new System.Drawing.Size(100, 20);
+            this.txtBox2.TabIndex = 4;
+            // 
+            // txtBox3
+            // 
+            this.txtBox3.Location = new System.Drawing.Point(68, 220);
+            this.txtBox3.Name = "txtBox3";
+            this.txtBox3.Size = new System.Drawing.Size(100, 20);
+            this.txtBox3.TabIndex = 5;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(204, 282);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 6;
+            this.btn1.Text = "Пресметни";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(266, 208);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Лице =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(266, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Периметър = ";
+            // 
+            // lice
+            // 
+            this.lice.AutoSize = true;
+            this.lice.Location = new System.Drawing.Point(381, 208);
+            this.lice.Name = "lice";
+            this.lice.Size = new System.Drawing.Size(0, 13);
+            this.lice.TabIndex = 9;
+            this.lice.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // perimetar
+            // 
+            this.perimetar.AutoSize = true;
+            this.perimetar.Location = new System.Drawing.Point(381, 154);
+            this.perimetar.Name = "perimetar";
+            this.perimetar.Size = new System.Drawing.Size(0, 13);
+            this.perimetar.TabIndex = 10;
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(12, 147);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(14, 13);
+            this.lblA.TabIndex = 11;
+            this.lblA.Text = "A";
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(12, 185);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(14, 13);
+            this.lblB.TabIndex = 12;
+            this.lblB.Text = "B";
+            // 
+            // lblC
+            // 
+            this.lblC.AutoSize = true;
+            this.lblC.Location = new System.Drawing.Point(12, 227);
+            this.lblC.Name = "lblC";
+            this.lblC.Size = new System.Drawing.Size(14, 13);
+            this.lblC.TabIndex = 13;
+            this.lblC.Text = "C";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(341, 282);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Изчисти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 335);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblC);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.perimetar);
+            this.Controls.Add(this.lice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txtBox3);
+            this.Controls.Add(this.txtBox2);
+            this.Controls.Add(this.txtBox1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.TextBox txtBox2;
+        private System.Windows.Forms.TextBox txtBox3;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lice;
+        private System.Windows.Forms.Label perimetar;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label lblC;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ C# Projects/Calculating Figures/radioUpr/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace radioUpr
+{
+    public partial class Form1 : Form
+    {
+        int button = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Functional Methods
+
+        public void ClearAll()
+        {
+            this.lice.Text = "";
+            this.perimetar.Text = "";
+            this.txtBox1.Text = "";
+            this.txtBox2.Text = "";
+            this.txtBox3.Text = "";
+
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            txtBox2.Hide();
+            lblB.Hide();
+            txtBox3.Hide();
+            lblC.Hide();
+            this.button = 1;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            txtBox2.Show();
+            txtBox3.Hide();
+            lblC.Hide();
+            lblB.Show();
+            this.button = 2;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            txtBox2.Show();
+            lblB.Show();
+            lblC.Show();
+            txtBox3.Show();
+            this.button = 2;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            
+            //
+            //
+            if(this.button==1)
+            {
+                int a = int.Parse(txtBox1.Text);
+                perimetar.Text = (4 * a).ToString();
+                lice.Text = (a * a).ToString();
+            }
+
+            if (this.button == 2)
+            {
+                int a = int.Parse(txtBox1.Text);
+                int b = int.Parse(txtBox2.Text);
+                perimetar.Text = (2*a + 2*b).ToString();
+                lice.Text = (a * b).ToString();
+            }
+            if(this.button==3)
+            {
+                int a = int.Parse(txtBox1.Text);
+                int b = int.Parse(txtBox2.Text);
+               int c = int.Parse(txtBox3.Text);
+                int p = (a + b + c);
+               perimetar.Text =p.ToString();
+               lice.Text = Math.Sqrt((p-a)*(p-b)*(p-c)).ToString();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.ClearAll();
+        }
+    }
+}
